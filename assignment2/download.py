fb0956a252f19500fe80ec0a544bc256560a0394
Qilong Zhao <qilongz@student.unimelb.edu.au>
2018-05-08 10:16:49 +1000

    11 load

--- assignment2/download.py
+++ assignment2/download.py
@@ -6,5 +6,6 @@ from hdfs import InsecureClient
 hdfs = InsecureClient('http://115.146.86.32:50070', user='qilongz')
 for i in hdfs.list('/team40/user_search_data'):
     hdfs_path = '/team40/user_search_data/'+i
-    local_path = '/vdc/team40/saved_search_data/'
-    hdfs.download(hdfs_path, local_path, overwrite=False, n_threads=5, temp_dir='tempFolder')
+    local_path = '/vdc/team40/searched_data/' + i
+    if os.path.isfile(local_path) == False:
+        hdfs.download(hdfs_path, local_path, overwrite=True, n_threads=5, temp_dir='temp')
